2b65df0e4746360c3aa3f827fe103fc882b0d925
DeveloperDurp <developerdurp@durp.info>
2023-08-06 17:01:50 +0000

    Update 5 files
    - /terraform/defaults.tf
    - /terraform/main.tf
    - /terraform/variables.tf
    - /terraform/terraform.tfvars
    - /.gitlab-ci.yml

---
+++ terraform/main.tf
@@ -0,0 +1,88 @@
+#k3s
+#-----------------------------------------------------
+
+resource "proxmox_vm_qemu" "k3master" {
+  count       = var.k3master.count
+  ciuser      = "administrator"
+  vmid        = "20${var.k3master.ip[count.index]}"
+  name        = var.k3master.name[count.index]
+  target_node = var.k3master.node[count.index]
+  clone       = var.k3master.template[count.index]
+  qemu_os     = "other"
+  full_clone  = true
+  os_type     = "cloud-init"
+  agent       = 1
+  cores       = var.k3master.cores
+  sockets     = 1
+  cpu         = "host"
+  memory      = var.k3master.memory
+  scsihw      = "virtio-scsi-pci"
+  bootdisk    = "scsi0"
+  boot        = "c"
+  onboot      = true
+  disk {
+    size    = var.k3master.drive
+    type    = "scsi"
+    format  = "raw"
+    storage = var.k3master.storage
+    ssd     = 1
+    backup  = false
+  }
+  network {
+    model  = "virtio"
+    bridge = "vmbr1"
+  }
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+  #Cloud Init Settings
+  ipconfig0    = "ip=192.168.20.${var.k3master.ip[count.index]}/24,gw=192.168.20.1"
+  searchdomain = "durp.loc"
+  nameserver   = var.dnsserver
+  sshkeys      = var.sshkeys
+}
+
+resource "proxmox_vm_qemu" "k3server" {
+  count       = var.k3server.count
+  ciuser      = "administrator"
+  vmid        = "20${var.k3server.ip[count.index]}"
+  name        = var.k3server.name[count.index]
+  target_node = var.k3server.node[count.index]
+  clone       = var.k3server.template[count.index]
+  qemu_os     = "other"
+  full_clone  = true
+  os_type     = "cloud-init"
+  agent       = 1
+  cores       = var.k3server.cores
+  sockets     = 1
+  cpu         = "host"
+  memory      = var.k3server.memory
+  scsihw      = "virtio-scsi-pci"
+  bootdisk    = "scsi0"
+  boot        = "c"
+  onboot      = true
+  disk {
+    size    = var.k3server.drive
+    type    = "scsi"
+    format  = "raw"
+    storage = var.k3server.storage
+    ssd     = 1
+    backup  = false
+  }
+  network {
+    model  = "virtio"
+    bridge = "vmbr1"
+  }
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+  #Cloud Init Settings
+  ipconfig0    = "ip=192.168.20.${var.k3server.ip[count.index]}/24,gw=192.168.20.1"
+  searchdomain = "durp.loc"
+  nameserver   = var.dnsserver
+  sshkeys      = var.sshkeys
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,55 @@
+variable "pm_api_url" {
+  description = "API URL to Proxmox provider"
+  type        = string
+}
+
+variable "dnsserver" {
+  description = "DNS provider"
+  type        = string
+}
+
+variable "sshkeys" {
+  description = "Public SSH key to inject into CloudInit"
+  type        = string
+}
+
+variable "pm_password" {
+  description = "Passowrd to Proxmox provider"
+  type        = string
+}
+
+variable "pm_user" {
+  description = "UIsername to Proxmox provider"
+  type        = string
+  default     = "root@pam"
+}
+
+variable "k3master" {
+  description = "Defaults of master nodes in K3S"
+  type = object({
+    count    = number
+    name     = list(string)
+    cores    = number
+    memory   = number
+    drive    = string
+    storage  = string
+    template = string
+    node     = list(string)
+    ip       = list(number)
+  })
+}
+
+variable "k3server" {
+  description = "Defaults of master nodes in K3S"
+  type = object({
+    count    = number
+    name     = list(string)
+    cores    = number
+    memory   = number
+    drive    = string
+    storage  = string
+    template = string
+    node     = list(string)
+    ip       = list(number)
+  })
+}
