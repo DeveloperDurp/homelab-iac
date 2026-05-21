5a38f7a82d3bbc3b288d1a14cffd1e2b01227fb0
DeveloperDurp <DeveloperDurp@durp.info>
2026-05-21 05:47:00 -0500

    update

--- dev/main.tf
+++ dev/main.tf
@@ -9,12 +9,13 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_parallel     = 1
-  pm_tls_insecure = true
-  pm_api_url      = var.pm_api_url
-  pm_user         = var.pm_user
-  pm_password     = var.pm_password
-  pm_debug        = false
+  pm_parallel                 = 1
+  pm_tls_insecure             = true
+  pm_api_url                  = var.pm_api_url
+  pm_user                     = var.pm_user
+  pm_password                 = var.pm_password
+  pm_debug                    = false
+  pm_minimum_permission_check = false
 }
 
 locals {
