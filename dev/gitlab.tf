7dad3534a4579ef6b2dfb4cb8fc64b042a97d199
DeveloperDurp <DeveloperDurp@durp.info>
2026-05-23 08:17:41 -0500

    update

--- dev/gitlab.tf
+++ dev/gitlab.tf
@@ -1,12 +1,12 @@
 resource "gitlab_project_variable" "talosconfig" {
-  project       = var.gitlab_project_id
+  project       = "7"
   key           = "DEV_TALOSCONFIG"
   value         = data.talos_client_configuration.talosconfig.talos_config
   variable_type = "file"
 }
 
 resource "gitlab_project_variable" "kubeconfig" {
-  project       = var.gitlab_project_id
+  project       = "7"
   key           = "DEV_KUBECONFIG"
   value         = resource.talos_cluster_kubeconfig.kubeconfig.kubeconfig_raw
   variable_type = "file"
