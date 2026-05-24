dbe72afb61961be0bf814271575d44857502a4fb
DeveloperDurp <DeveloperDurp@durp.info>
2026-05-24 09:13:51 -0500

    update

--- dev/unifi.tf
+++ dev/unifi.tf
@@ -1,6 +1,6 @@
 provider "unifi" {
-  api_url  = "https://192.168.1.1/"
-  api_key  = var.unifi_api_key
+  api_url        = "https://192.168.1.1/"
+  api_key        = var.unifi_api_key
   allow_insecure = true
 }
 
@@ -10,28 +10,28 @@ variable "unifi_api_key" {
 }
 
 resource "unifi_dns_record" "control_records" {
-  count       = local.control.count
-  name        = "${local.control.name[count.index]}.durp.loc"
-  type        = "A"
-  value       = local.control.ip[count.index]
-  enabled     = true
-  ttl         = 300
+  count   = local.control.count
+  name    = "${local.control.name[count.index]}.durp.loc"
+  type    = "A"
+  value   = local.control.ip[count.index]
+  enabled = true
+  ttl     = 300
 }
 
 resource "unifi_dns_record" "worker_records" {
-  count       = local.worker.count
-  name        = "${local.worker.name[count.index]}.durp.loc"
-  type        = "A"
-  value       = local.worker.ip[count.index]
-  enabled     = true
-  ttl         = 300
+  count   = local.worker.count
+  name    = "${local.worker.name[count.index]}.durp.loc"
+  type    = "A"
+  value   = local.worker.ip[count.index]
+  enabled = true
+  ttl     = 300
 }
 
 resource "unifi_dns_record" "cluster_endpoint" {
-  count       = local.control.count
-  name        = local.talos.cluster_dns
-  type        = "A"
-  value       = local.control.ip[count.index]
-  enabled     = true
-  ttl         = 300
+  count   = local.control.count
+  name    = local.talos.cluster_dns
+  type    = "A"
+  value   = local.control.ip[count.index]
+  enabled = true
+  ttl     = 300
 }
