02bc3383437680b99d67e85594a40cc4c66452e5
Durp <developerdurp@durp.info>
2026-06-13 15:38:08 +0000

    Update 2 files
    - /iac_repo/infra/talos.tf
    - /iac_repo/infra-apps/.gitlab/.gitlab-ci.yml

--- iac_repo/infra/talos.tf
+++
@@ -1,7 +0,0 @@
-resource "unifi_dns_record" "cluster_vip_dns" {
-  name        = local.talos.cluster_dns
-  record_type = "A"
-  value       = local.talos.cluster_vip
-  enabled     = true
-  ttl         = 300
-}
